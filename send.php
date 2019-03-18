b7105f1a7e71a804055447b264cebc8c8bb79d13
YevhenDemchenko <45935351+YevhenDemchenko@users.noreply.github.com>
2019-03-18 13:42:56 +0200

    Update send.php

--- send.php
+++ send.php
@@ -1,22 +1,47 @@
-<?php
-	$to = "jkl130773@gmail.com"; 
-	$from = $_POST['email']; 
-	$name = $_POST['name'];
-	$subject = "Форма отправки сообщений с сайта портфолио";
-	$subject2 = "Copy of your form submission";
-	$message = $name . " оставил сообщение:" . "\n\n" . $_POST['message'];
-	$message2 = "Here is a copy of your message " . $name . "\n\n" . $_POST['message'];
-	$headers = "From:" . $from;
-	$headers2 = "From:" . $to;
-	mail($to,$subject,$message,$headers);
-	// mail($from,$subject2,$message2,$headers2); // sends a copy of the message to the sender - Отключено!
-	echo "Сообщение отправлено. Спасибо Вам " . $name . ".";
-	echo "<br /><br /><a href='https://epicblog.net'>Вернуться на сайт.</a>";
+<?php 
+
+require_once('phpmailer/PHPMailerAutoload.php');
+$mail = new PHPMailer;
+$mail->CharSet = 'utf-8';
+
+$name = $_POST['name'];
+$message = $_POST['message'];
+$email = $_POST['email'];
+
+//$mail->SMTPDebug = 3;                               // Enable verbose debug output
+
+$mail->isSMTP();                                      // Set mailer to use SMTP
+$mail->Host = 'smtp-relay.gmail.com';  																							// Specify main and backup SMTP servers
+$mail->SMTPAuth = true;                               // Enable SMTP authentication
+$mail->Username = 'postportfolio13@gmail.com'; // Ваш логин от почты с которой будут отправляться письма
+$mail->Password = '98omovep'; // Ваш пароль от почты с которой будут отправляться письма
+$mail->SMTPSecure = 'ssl';                            // Enable TLS encryption, `ssl` also accepted
+$mail->Port = 465; // TCP port to connect to / этот порт может отличаться у других провайдеров
+
+$mail->setFrom('postportfolio13@gmail.com'); // от кого будет уходить письмо?
+$mail->addAddress('jkl130773@gmail.com');     // Кому будет уходить письмо 
+//$mail->addAddress('ellen@example.com');               // Name is optional
+//$mail->addReplyTo('info@example.com', 'Information');
+//$mail->addCC('cc@example.com');
+//$mail->addBCC('bcc@example.com');
+//$mail->addAttachment('/var/tmp/file.tar.gz');         // Add attachments
+//$mail->addAttachment('/tmp/image.jpg', 'new.jpg');    // Optional name
+$mail->isHTML(true);                                  // Set email format to HTML
+
+$mail->Subject = 'Письмо с сайта портфолио';
+$mail->Body    = '' .$name . ' написал письмо ' .$message. '<br>Почта этого пользователя: ' .$email;
+$mail->AltBody = '';
+
+if(!$mail->send()) {
+    echo 'Error';
+} else {
+    echo "Сообщение отправлено. Спасибо Вам " . $name . ".";
+}
 ?>
 
 <!--Переадресация на главную страницу сайта, через 3 секунды-->
 <script language="JavaScript" type="text/javascript">
-function changeurl(){eval(self.location="https://epicblog.net/index.php");}
+function changeurl(){eval(self.location="https://yevhendemchenko.github.io/");}
 window.setTimeout("changeurl();",3000);
 history.replaceState({ searchTerm: searchBox.val(), page: pageNumber, loaded: 0 }, "", document.URL);
 </script>
