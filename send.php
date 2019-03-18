5d4046814539962ebff650d90339daa83504c5a3
YevhenDemchenko <45935351+YevhenDemchenko@users.noreply.github.com>
2019-03-18 13:13:13 +0200

    Update send.php

--- send.php
+++ send.php
@@ -1,19 +1,17 @@
 <?php
-if(isset($_POST['submit'])){
 	$to = "jkl130773@gmail.com"; 
 	$from = $_POST['email']; 
-	$first_name = $_POST['first_name'];
-	$subject = "Форма отправки сообщений с сайта";
+	$name = $_POST['name'];
+	$subject = "Форма отправки сообщений с сайта портфолио";
 	$subject2 = "Copy of your form submission";
-	$message = $first_name . " оставил сообщение:" . "\n\n" . $_POST['message'];
-	$message2 = "Here is a copy of your message " . $first_name . "\n\n" . $_POST['message'];
+	$message = $name . " оставил сообщение:" . "\n\n" . $_POST['message'];
+	$message2 = "Here is a copy of your message " . $name . "\n\n" . $_POST['message'];
 	$headers = "From:" . $from;
 	$headers2 = "From:" . $to;
 	mail($to,$subject,$message,$headers);
 	// mail($from,$subject2,$message2,$headers2); // sends a copy of the message to the sender - Отключено!
-	echo "Сообщение отправлено. Спасибо Вам " . $first_name . ".";
+	echo "Сообщение отправлено. Спасибо Вам " . $name . ".";
 	echo "<br /><br /><a href='https://epicblog.net'>Вернуться на сайт.</a>";
-}
 ?>
 
 <!--Переадресация на главную страницу сайта, через 3 секунды-->
